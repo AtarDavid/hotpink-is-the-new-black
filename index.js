70d870b3f428e7ca1f57e7c18dd9e4ab78de83f8
AD <atar.dci@gmail.com>
2019-07-09 14:11:24 +0200

    Refactor all functions to accept dynamic parameters

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const app = express();
 const port = 4000;
 const convert = require('color-convert');
 
-app.get('/convert/rgbtohsl/', (req, res) =>
-  res.send(convert.rgb.hsl(255, 105, 180))
+app.get('/convert/rgbtohsl', (req, res) => {
+  let rgbColor = req.query.color.split(',').map(color => parseInt(color));
+  res.send(convert.rgb.hsl(rgbColor));
+});
+app.get('/convert/keywordtorgb', (req, res) =>
+  res.send(convert.keyword.rgb(req.query.color))
 );
-app.get('/convert/keywordtorgb/', (req, res) =>
-  res.send(convert.keyword.rgb('hotpink'))
-);
-app.get('/convert/rgbtohex/', (req, res) =>
-  res.send(convert.rgb.hex(255, 105, 180))
-);
-app.get('/convert/hextorgb/', (req, res) =>
-  res.send(convert.hex.rgb('FF69B4'))
+app.get('/convert/rgbtohex', (req, res) => {
+  let rgbColor = req.query.color.split(',').map(color => parseInt(color));
+  res.send(convert.rgb.hex(rgbColor));
+});
+app.get('/convert/hextorgb', (req, res) =>
+  res.send(convert.hex.rgb(req.query.color))
 );
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
