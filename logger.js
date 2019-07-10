9ea165bfd10e57aaef59b3722548b05660f129f3
AD <atar.dci@gmail.com>
2019-07-10 14:36:07 +0200

    Slice time string

--- logger.js
+++ logger.js
@@ -1,6 +1,6 @@
 module.exports = logger = (req, res, next) => {
   const time = new Date();
   const { method, path } = req;
-  console.log(`Request ${method} on ${path} ${time}`);
+  console.log(`Request ${method} on ${path} ${time.toString().slice(0, 33)}`);
   next();
 };
