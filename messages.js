ad1443075e1a4c9194e019d76e74af9a1bd5bb58
AD <atar.dci@gmail.com>
2019-07-11 10:04:34 +0200

    Add commented explanations

--- messages.js
+++ messages.js
@@ -1,7 +1,10 @@
 module.exports = errHandler = (req, res, next) => {
+  // Check if the query parameter color is part of the request
   if (!req.query.color) {
+    // If not - respond with an error message
     res.status(400).send('Missing query parameter color');
   } else {
+    // Otherwise continue
     next();
   }
 };
